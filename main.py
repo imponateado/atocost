2c1ac7686c7dcaf090abfbc191ccdbf8056c00cd
Leonardo Teodoro <38961152+imponateado@users.noreply.github.com>
2022-09-23 14:43:52 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from PIL import Image
+import pytesseract
+import os
+from pdf2image import convert_from_path
+import re
+
+
+files = os.listdir()
+
+for i in files:
+    pages = convert_from_path(i, 500)
+    for j in pages:
+        output = pytesseract.image_to_string(j)
+        print(output)
+        print("---------------------------------------")
+        n = output.find("Tot. ped.")   
+        renomear = output[n+9:n+14]
+        os.rename(i, renomear+".pdf")
